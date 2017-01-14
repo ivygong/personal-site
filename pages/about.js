00169ec2ebe0c74849ac69410ecaf166c24a70d0
Ivy Gong <ivygong@Ivys-MacBook-Pro.local>
2017-01-13 19:39:10 -0800

    first commit

--- pages/about.js
+++ pages/about.js
@@ -15,11 +15,35 @@ export default class About extends Component {
           <div className="about__title">
             About
           </div>
-          <div className="about__section">
-            <div className="about__image">
-              <img src={prefixLink('/img/aboutpicture.jpg')}/>
+          <div className="about__body">
+            <div className="about__section">
+              <div className="about__image">
+                <img src={prefixLink('/img/aboutpicture.jpg')}/>
+              </div>
+              <div className="about__summary">Hi. I'm Ivy. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+              <div className="about__links">
+                <a href={prefixLink('/pdf/resume.pdf')} target="_blank">
+                  <div className="about__link--block">
+                    Resume
+                  </div>
+                </a>
+                <a href="https://www.google.com">
+                  <div className="about__link--block">
+                    LinkedIn
+                  </div>
+                </a>
+                <a href="www.google.com">
+                  <div className="about__link--block">
+                    Email
+                  </div>
+                </a>
+                <a href="https://github.com/ivygong">
+                  <div className="about__link--block">
+                    Github
+                  </div>
+                </a>
+              </div>
             </div>
-            <div className="about__summary">Hi. I'm Ivy.</div>
           </div>
         </div>
       </div>
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -8,11 +8,18 @@ export default class Portfolio extends Component {
   render () {
     const data = [
       {
-        name: 'San Diego World Affairs Council',
-        link: 'sdwac.org',
-        website: 'http://sdwac.org/',
-        imgurl: '/img/portfolio-sdwac.jpg',
-        summary:'The San Diego World Affairs Council (SDWAC) was founded in 1954. SDWAC is part of the World Affairs Councils of America, the largest international affairs non-profit organization in the United States. In addition, we have a North County Chapter serving that area and through our combined efforts we serve over three hundred members. SDWAC promotes interest and dialogue throughout the San Diego community in international relations and world affairs. Because SDWAC is a non-profit that tries to promote discussion of relevant issues in San Diego, it holds events very often. We implemented a modern, extremely functional calendar to highlight that, in addition to creating an entire events section.'
+        name: 'Pharmapace',
+        link: 'pharmapace.com',
+        website: 'http://www.pharmapace.com/',
+        imgurl: '/img/portfolio-pharmapace.jpg',
+        summary: 'TEMP'
+      },
+      {
+        name: 'A Week in the Wizarding World',
+        link: 'hpsummercamp.com',
+        website: 'http://www.hpsummercamp.com/',
+        imgurl: '/img/portfolio-hp.jpg',
+        summary: 'A Week in the Wizarding World is a summer camp program for fourth and fifth graders. Students get to experience the magic of Harry Potter with a large variety of themed activities, including House Sorting, Butterbeer, and the End of the Year Feast. There were two important aspects to emphasize here: the unfortunately necessary practicalities and the magic of Harry Potter. Complicated processes like registration became streamlined with simple online forms on the Register Now page. The wonder of the HP world wasn’t lost though; sliders and graphics allowed for plenty of visuals that conveyed the fun side of the camp.'
       },
       {
         name: 'Hamilton College Consulting',
@@ -22,11 +29,11 @@ export default class Portfolio extends Component {
         summary:'For Christopher Hamilton and the HCC team, education is not a business — it is a calling. Offering highly personalized counseling and inspiring teaching methods focused on building foundational skills, Hamilton College Consulting endeavors to inspire more critical and innovative thinkers. At Ermana, we wanted to make sure that HCC’s student-first commitment was reflected in their website. Features like the voices page ensured that no one could mistake HCC for another education company interested only in test scores and statistics.' 
       },
       {
-        name: 'A Week in the Wizarding World',
-        link: 'hpsummercamp.com',
-        website: 'http://www.hpsummercamp.com/',
-        imgurl: '/img/portfolio-hp.jpg',
-        summary: 'A Week in the Wizarding World is a summer camp program for fourth and fifth graders. Students get to experience the magic of Harry Potter with a large variety of themed activities, including House Sorting, Butterbeer, and the End of the Year Feast. There were two important aspects to emphasize here: the unfortunately necessary practicalities and the magic of Harry Potter. Complicated processes like registration became streamlined with simple online forms on the Register Now page. The wonder of the HP world wasn’t lost though; sliders and graphics allowed for plenty of visuals that conveyed the fun side of the camp.'
+        name: 'San Diego World Affairs Council',
+        link: 'sdwac.org',
+        website: 'http://sdwac.org/',
+        imgurl: '/img/portfolio-sdwac.jpg',
+        summary:'The San Diego World Affairs Council (SDWAC) was founded in 1954. SDWAC is part of the World Affairs Councils of America, the largest international affairs non-profit organization in the United States. In addition, we have a North County Chapter serving that area and through our combined efforts we serve over three hundred members. SDWAC promotes interest and dialogue throughout the San Diego community in international relations and world affairs. Because SDWAC is a non-profit that tries to promote discussion of relevant issues in San Diego, it holds events very often. We implemented a modern, extremely functional calendar to highlight that, in addition to creating an entire events section.'
       }
     ];
 
